afe61aaf0dda9051d0f447a1af0718bdc6f6ceb7
Darryl <darryl1975@hotmail.com>
2023-08-01 16:29:22 +0800

    workshop day 31 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { FruitsComponent } from './components/fruits/fruits.component';
 import { DataEntryComponent } from './data-entry/data-entry.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { CartListComponent } from './cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FruitsComponent,
     DataEntryComponent,
-    DataDisplayComponent
+    DataDisplayComponent,
+    ItemListComponent,
+    ItemsListComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule
