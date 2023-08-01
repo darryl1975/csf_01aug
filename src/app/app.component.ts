0c052a75ad3867701e738094ae4e96536fa32c9c
Darryl <darryl1975@hotmail.com>
2023-08-01 14:32:28 +0800

    complete lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,19 @@ export class AppComponent {
   title = 'csf_01aug';
   updatedNumber: number;
 
+  leftEnabled: boolean = true;
+  rightEnabled: boolean = true;
+
   changedNumber(eventNumber: number) {
     this.updatedNumber = eventNumber;
     console.log('parent number: ' + this.updatedNumber);
+  }
+
+  ToggleLeft() {
+    this.leftEnabled = !this.leftEnabled;
+  }
+
+  ToggleRight() {
+    this.rightEnabled = !this.rightEnabled;
   }
 }
--- src/app/components/fruits/fruits.component.ts
+++ src/app/components/fruits/fruits.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fruits',
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.css']
 })
-export class FruitsComponent {  
+export class FruitsComponent implements OnInit {
+
   fruit: string = 'Durian'; 
   
   fullName: string = 'Darryl Ng';
@@ -14,6 +15,14 @@ export class FruitsComponent {
 
   // xx-small, x-small, small, medium, large, x-large, xx-large
   fontSize: string = 'xxx-large';
+
+  fruits : [ { name: string, quantity: number } ] = [ { name: 'apple', quantity: 50 }];
+
+  ngOnInit(): void {
+    this.fruits.push({ name: 'mango', quantity: 10 });
+    this.fruits.push({ name: 'durian', quantity: 10 });
+    this.fruits.push({ name: 'avocado', quantity: 25 });
+  } 
 
   testClick() {
     console.log('Button Test Clicked');
